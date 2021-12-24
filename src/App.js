e82ec0e67a1f554113c18bfaf195ece2dfbbf39e
Chandni Singh <chandni1141996@gmail.com>
2021-12-24 20:23:17 +0530

    refactoring and styling search box

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import CardList from './components/card-list/card-list';
+import SearchBox from './components/serach-box/search-box';
 import './App.css';
 
 class App extends React.Component {
@@ -18,7 +19,7 @@ class App extends React.Component {
     const filteredMonsters = this.state.monsters.filter(monster => monster.name.toLowerCase().includes(this.state.searchField.toLowerCase()));
     return (
       <div className='App'>
-        <input type='search' placeholder='search monsters...' onChange={e => this.setState({searchField : e.target.value})} />
+        <SearchBox placeholder='search monsters...' handleChange={e => this.setState({searchField : e.target.value})} />
         <CardList monsters={filteredMonsters} />
       </div>
     )
